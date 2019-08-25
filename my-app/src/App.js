14e1fe3d76d6288b682a20196a44bf2d2a1506be
raouiafredj <raouianour@gmail.com>
2019-08-25 15:39:33 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Components/MovieApp';
+import MovieApp from './Components/MovieApp';
+
+
+function App() {
+  return (
+    <div>
+
+          <MovieApp/>
+
+
+    </div>
+    
+  );
+}
+
+export default App;
